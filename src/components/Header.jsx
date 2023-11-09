fb374ce7672802b96128c8715d1dbfabbdf9a9df
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-10 00:07:39 +1100

    Add instructions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 function Header({ score, bestScore }) {
   return (
-    <header>
-      <h1>Memory Match</h1>
-      <div className="scores">
-        <h2 className="score">Score: {score}</h2>
-        <h2 className="best-score">Best Score: {bestScore}</h2>
+    <>
+      <header>
+        <h1>Memory Match</h1>
+        <div className="scores">
+          <h2 className="score">Score: {score}</h2>
+          <h3 className="best-score">Best Score: {bestScore}</h3>
+        </div>
+      </header>
+      <div className="instructions">
+        <p>
+          Score points by clicking images, but avoid selecting the same image
+          twice!
+        </p>
       </div>
-    </header>
+    </>
   );
 }
 
